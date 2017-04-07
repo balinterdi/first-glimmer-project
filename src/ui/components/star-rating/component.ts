ac84319888f9da185f027eb04e04eb7cf1651839
Balint Erdi <balint.erdi@gmail.com>
2017-04-07 12:03:08 +0200

    Stars are displayed according to the passed `rating` value

--- src/ui/components/star-rating/component.ts
+++ src/ui/components/star-rating/component.ts
@@ -1,5 +1,29 @@
-import Component from '@glimmer/component';
+import Component, { tracked } from '@glimmer/component';
 
 export default class StarRating extends Component {
-  stars = [1, 2, 3];
+
+  /*
+  args: {
+    todo: Todo;
+    onEdit: (Todo, string) => void;
+    onToggle: (Todo) => void;
+    onDestroy: (Todo) => void;
+  };
+   */
+
+  @tracked('args')
+  get stars() {
+    let rating = Math.round(this.args.rating);
+    let fullStars = this.starRange(1, rating, 'full');
+    let emptyStars = this.starRange(rating + 1, this.args.maxRating, 'empty');
+    return fullStars.concat(emptyStars);
+  };
+
+  starRange: function(start, end, type) {
+    let starsData = [];
+    for (var i = start; i <= end; i++) {
+      starsData.push({ rating: i, full: type === 'full' });
+    }
+    return starsData;
+  };
 };
